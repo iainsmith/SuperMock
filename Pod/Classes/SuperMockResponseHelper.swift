9bfba2be76597fcee4eb57ff80e815260cea99f2
Iain Smith <iainsmith@users.noreply.github.com>
2015-11-27 14:36:12 +0000

    Add strict mocks option.

--- Pod/Classes/SuperMockResponseHelper.swift
+++ Pod/Classes/SuperMockResponseHelper.swift
@@ -103,7 +103,11 @@ public class SuperMockResponseHelper: NSObject {
     
     private func mockURLForRequestURL(url: NSURL, requestMethod: RequestMethod, mocks: Dictionary<String,AnyObject>) -> NSURL? {
         guard let definitionsForMethod = mocks[requestMethod.rawValue] as? Dictionary<String,AnyObject> else {
-            return url
+            if SuperMockConfig.sharedConfig.useStrictMocks {
+                fatalError("Couldn't find definitions for request: \(requestMethod) make sure to create a node for it in the plist")
+            } else {
+                return url
+            }
         }
 
         let trasformedURL: NSURL
